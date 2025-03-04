e06541b960c3cca63297f57f5a72990ef69d0d95
kaitlyyynh <kh3599@nyu.edu>
2025-03-04 18:32:58 -0500

    add updating roles to frontend via an add role EP call

--- src/Components/PeopleDetails/PeopleDetails.jsx
+++ src/Components/PeopleDetails/PeopleDetails.jsx
@@ -13,12 +13,14 @@ function PeopleDetails() {
   const [person, setPerson] = useState(initialPerson);
   const [newName, setNewName] = useState(person?.name || '');
   const [newAffiliation, setNewAffiliation] = useState(person?.affiliation || '');
+  const [newRole, setNewRole] = useState(person?.role || '')
   const [error, setError] = useState('');
 
   const PERSON_EP = `${BACKEND_URL}/people/${initialPerson.email}`
   const UPDATE_NAME_EP =`${BACKEND_URL}/people/updateName/${person.email}/${newName}`
   const UPDATE_AFFIL_EP =`${BACKEND_URL}/people/updateAffiliation/${person.email}/${newAffiliation}`
-
+  const UPDATE_ROLE_EP = `${BACKEND_URL}/people/${person.email}/addRole/${newRole}`
+  // @api.route(f'{PEOPLE_EP}/<string:email>/addRole/<string:role>')
   useEffect(() => {
     if (!initialPerson?.email) return;
 
@@ -65,6 +67,21 @@ function PeopleDetails() {
       });
     };
 
+    const UpdateRole = () => {
+      axios
+        .put(UPDATE_ROLE_EP)
+        .then(() => {
+          navigate('/people'); // redirect after updating
+        })
+        .catch((error) => {
+          if (error.response && error.response.data && error.response.data.message) {
+            setError(`Error: ${error.response.data.message}`);
+          } else {
+            setError(`There was an unexpected error with updating role. ${error}`);
+          }
+        });
+      };
+
   return (
     <div>
       <h1>Person Details</h1>
@@ -98,7 +115,19 @@ function PeopleDetails() {
           />
           <button type="button" onClick={UpdateAffiliation}>Update Affiliation</button>
         </div>
+        {/* Update Affiliation */}
       </form>
+      {/* Update Role(s) */}
+      <label htmlFor="role">Enter new Role:</label>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <input
+            type="text"
+            id="role"
+            value={newRole}
+            onChange={(e) => setNewRole(e.target.value)}
+          />
+          <button type="button" onClick={UpdateRole}>Update Role</button>
+        </div>
       {error && <p style= {{color:'red'}}>{error}</p>}
     </div>
   );
